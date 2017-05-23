8b626944702632df59bb97504017e688944bd450
abhi <abhidashing.singh@gmail.com>
2017-05-23 11:58:43 +0530

    Adding Login Screen

--- app/src/main/java/com/moonbeam/qrattendance/Main2Activity.java
+++ app/src/main/java/com/moonbeam/qrattendance/Main2Activity.java
@@ -8,6 +8,15 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.ProtocolException;
 import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
@@ -26,10 +35,18 @@ public class Main2Activity extends AppCompatActivity {
         WebSettings webSettings = webv.getSettings();
         webSettings.setJavaScriptEnabled(true);
     }
-    public void onButtonClick(View v){
+    public void onButtonClick(View v) throws IOException{
         if(v.getId()==R.id.button){
             String name=(String) nameview.getText();
             String pass=(String)passwordview.getText();
+            String wsite="demotestsocial.000webhostapp.com/login.php?name="+name+"&pass="+pass;
+            URL url = new URL(wsite);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String response=reader.readLine();
+            Toast.makeText(Main2Activity.this, response,Toast.LENGTH_LONG).show();
             Intent i=new Intent(Main2Activity.this,MainActivity.class);
             startActivity(i);
         }
