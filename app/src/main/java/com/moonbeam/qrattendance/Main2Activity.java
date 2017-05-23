e2473f1a972d45b3ec738b4970317dcad1123b27
abhi <abhidashing.singh@gmail.com>
2017-05-23 15:58:00 +0530

    Adding Login Screen

--- app/src/main/java/com/moonbeam/qrattendance/Main2Activity.java
+++ app/src/main/java/com/moonbeam/qrattendance/Main2Activity.java
@@ -1,53 +1,66 @@
 package com.moonbeam.qrattendance;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.webkit.WebView;
-import android.webkit.WebSettings;
+import android.widget.EditText;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.HttpURLConnection;
-import java.io.InputStream;
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.ProtocolException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
     private Button btn;
-    private TextView nameview;
-    private TextView passwordview;
-    private WebView webv;
+    private EditText nameview;
+    private EditText passwordview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_main2);
-        btn=(Button)findViewById(R.id.button);
-        nameview=(TextView)findViewById(R.id.username);
-        passwordview=(TextView)findViewById(R.id.pass);
-        webv=(WebView)findViewById(R.id.login);
-        WebSettings webSettings = webv.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    login();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
-    public void onButtonClick(View v) throws IOException{
-        if(v.getId()==R.id.button){
-            String name=(String) nameview.getText();
-            String pass=(String)passwordview.getText();
-            String wsite="demotestsocial.000webhostapp.com/login.php?name="+name+"&pass="+pass;
-            URL url = new URL(wsite);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            InputStream in = new BufferedInputStream(conn.getInputStream());
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            String response=reader.readLine();
-            Toast.makeText(Main2Activity.this, response,Toast.LENGTH_LONG).show();
-            Intent i=new Intent(Main2Activity.this,MainActivity.class);
+    void login()throws IOException{
+        nameview = (EditText) findViewById(R.id.username);
+        passwordview = (EditText) findViewById(R.id.pass);
+        String name = nameview.getText().toString();
+        name = URLEncoder.encode(name, "UTF-8");
+        String pass = passwordview.getText().toString();
+        pass = URLEncoder.encode(pass, "UTF-8");
+        String response = "0";
+        String wsite = "http://demotestsocial.000webhostapp.com/login.php?name=" + name + "&pass=" + pass;
+        URL url = new URL(wsite);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("POST");
+        urlConnection.setDoOutput(true);
+        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        response = reader.readLine();
+        Toast.makeText(Main2Activity.this, response, Toast.LENGTH_LONG).show();
+        if (!response.equals("0")) {
+            Intent i = new Intent(Main2Activity.this, MainActivity.class);
+            i.putExtra("channel",response);
             startActivity(i);
         }
     }
